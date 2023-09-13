a573e3af1f507a44e8e9135da441eb319ab36e53
Ahmet <ahmetyilmaznnb2005@gmail.com>
2023-09-13 21:45:38 +0200

    erste commit

---
+++ src/test/java/com/oracleExam/OR01_Increment.java
@@ -0,0 +1,19 @@
+package com.oracleExam;
+
+public class OR01_Increment {
+    public static void main (String[] args) {
+
+        int num1= 9;
+
+        int num2 = num1++;
+
+        if(num2 < 10){
+            System.out.println( num2 + " Hello World");
+        } else {
+            System.out.println( num2 + "Hello Universal");
+        }
+
+
+
+    }
+}
